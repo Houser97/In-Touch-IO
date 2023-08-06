6d645c3b0fc7b29180b7e26e0bb4a29d072d3f40
Arturo Rivera <arturo.riverar97@gmail.com>
2023-08-06 11:40:09 -0600

    Set contextAPI, openChat/chatData states and components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,51 @@
+import React, { createContext, useState } from 'react'
 import './App.css'
+import Chat from './components/Chat'
+import ContactsCarousel from './components/ContactsCarousel'
+import Header from './components/Header'
+import MessagesList from './components/MessagesList'
+
+interface chatData_types {
+  image: string,
+  name: string
+}
+
+interface chatContext_types {
+  openChat: boolean,
+  chatData: chatData_types,
+  setOpenChat: React.Dispatch<React.SetStateAction<boolean>>,
+  setChatData: React.Dispatch<React.SetStateAction<chatData_types>>
+}
+
+export const chatContext = createContext<chatContext_types>({
+  openChat: false,
+  chatData: {
+    image: '',
+    name: ''
+  },
+  setOpenChat: () => false,
+  setChatData: () => false
+})
 
 function App() {
+
+  const [openChat, setOpenChat] = useState(false)
+  const [chatData, setChatData] = useState({
+    image: '',
+    name: ''
+  })
+
+  const values = {setOpenChat, openChat, chatData, setChatData}
+
   return (
-    <>
-    </>
+    <div className='App'>
+      <chatContext.Provider value={values}>
+        <Header />
+        <ContactsCarousel />
+        <MessagesList />
+        <Chat />
+      </chatContext.Provider>
+    </div>
   )
 }
 
