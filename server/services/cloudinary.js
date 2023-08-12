f3ac796cec2d3472dda0a656d3f3cbba9aba864d
Arturo Rivera <arturo.riverar97@gmail.com>
2023-08-12 14:43:30 -0600

    Create cloudinary file to set cloudinary object

---
+++ server/services/cloudinary.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const cloudinary = require('cloudinary').v2; // Importar directamente el módulo v2
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY_CLOUDINARY,
+    api_secret: process.env.API_SECRET_CLOUDINARY
+});
+
+module.exports = { cloudinary };
