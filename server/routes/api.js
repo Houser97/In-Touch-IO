0f9e4bf20a18213225291735b9790010c7009f70
Arturo Rivera <arturo.riverar97@gmail.com>
2023-08-13 19:54:36 -0600

    Adjust api routes and add middleware to validate token

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController')
+const userController = require('../controllers/userController');
 
 router.post('/signup', userController.check_email, userController.create_user);
 router.post('/login', userController.login);
-router.get('/get_user_data', userController.get_user_data);
+router.post('/get_user_data', userController.validate_token,userController.get_user_data);
+router.post('/upload_image/:id', userController.validate_token,userController.upload_image)
 
 
 
