2f5e4e25e7de69325fb9534a166e303a805453b2
Arturo Rivera <arturo.riverar97@gmail.com>
2023-08-19 13:57:31 -0600

    Fetch chat when clickin Contact component

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -1,8 +1,18 @@
+import { AccessChat } from '../assets/constants'
 import '../styles/Contact.css'
 
-const Contact = ({picture = '', name = ''}) => {
+const Contact = ({picture = '', name = '', id = ''}) => {
+
+  const handleClick = async() => {
+    const token = JSON.parse(localStorage.getItem('token') || '');
+    const userId = JSON.parse(localStorage.getItem('idInTouch') || "");
+    if(userId === id) return
+    const chat = await AccessChat(token, id)
+    console.log(chat)
+  }
+
   return (
-    <div className="contact">
+    <div className="contact" onClick={handleClick}>
         <img className='contact__picture' src={picture} alt="contact-picture" />
         {name.length !== 0 && <div className="contact__name">{name}</div>}
     </div>
