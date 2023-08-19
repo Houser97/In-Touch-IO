b8fda110d5040eaf41230f503f2984b12137f26b
Arturo Rivera <arturo.riverar97@gmail.com>
2023-08-18 20:37:52 -0600

    Create router object using react-router-dom

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
+import Authentication from './components/Authentication'
+import UpdateUser from './components/UpdateUser'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Authentication />,
+  },
+  {
+    path: '/chats',
+    element: <App />
+  },
+  {
+    path: '/settings/:id',
+    element: <UpdateUser />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
