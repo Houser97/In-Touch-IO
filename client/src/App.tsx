8a21f75526f23f41d33e18b4b2a18344d2529ba8
Arturo Rivera <arturo.riverar97@gmail.com>
2023-08-18 20:36:14 -0600

    Fetch user data when component mounts and set up types/user state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,46 +1,107 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './App.css'
 import Chat from './components/Chat'
 import ContactsCarousel from './components/ContactsCarousel'
-import Header from './components/Header'
+import HeaderMain from './components/HeaderMain'
 import MessagesList from './components/MessagesList'
 
-interface chatData_types {
+interface image_type {
+  url: string,
+  publicId: string,
+}
+
+interface user_type {
+  image: image_type,
+  name: string,
+  id: string
+}
+
+interface chatData_type {
   image: string,
-  name: string
+  name: string,
 }
 
 interface chatContext_types {
   openChat: boolean,
-  chatData: chatData_types,
+  user: user_type,
+  chatData: chatData_type,
   setOpenChat: React.Dispatch<React.SetStateAction<boolean>>,
-  setChatData: React.Dispatch<React.SetStateAction<chatData_types>>
+  setUser: React.Dispatch<React.SetStateAction<user_type>>,
+  setChatData: React.Dispatch<React.SetStateAction<chatData_type>>
 }
 
 export const chatContext = createContext<chatContext_types>({
   openChat: false,
+  user: {
+    image: {
+      url: '',
+      publicId: ''
+    },
+    name: '',
+    id: ''
+  },
   chatData: {
-    image: '',
-    name: ''
+    name: '',
+    image: ''
   },
   setOpenChat: () => false,
+  setUser: () => false,
   setChatData: () => false
 })
 
 function App() {
 
+  const navigate = useNavigate();
+
   const [openChat, setOpenChat] = useState(false)
+  //Estado que se usa para mostrar datos del contacto al abrir el chat.
   const [chatData, setChatData] = useState({
     image: '',
     name: ''
   })
+  const [user, setUser] = useState({
+    image: {
+      url: '',
+      publicId: ''
+    },
+    name: '',
+    id: '',
+  })
 
-  const values = {setOpenChat, openChat, chatData, setChatData}
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('token') || "");
+    const id = JSON.parse(localStorage.getItem('idInTouch') || "");
+    fetch(`http://localhost:3000/api/get_user_data`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify({id})
+    })
+    .then(response => response.json())
+    .then(user => {
+      if(!user) {
+        navigate('/')
+        return undefined
+      }
+      const { name, pictureUrl, publicId, _id } = user
+      const image = {
+        url: pictureUrl,
+        publicId
+      }
+      setUser({name, image, id: _id})
+    })
+  }, [])
+  
+
+  const values = {setOpenChat, openChat, user, setUser, chatData, setChatData}
 
   return (
     <div className='App'>
       <chatContext.Provider value={values}>
-        <Header />
+        <HeaderMain />
         <ContactsCarousel />
         <MessagesList />
         <Chat />
