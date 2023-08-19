156fb3ff651ba8439354168e28ef50a808cdd2af
Arturo Rivera <arturo.riverar97@gmail.com>
2023-08-19 13:58:12 -0600

    Create AccessChat function to call API to retrieve chat data

--- client/src/assets/constants.tsx
+++ client/src/assets/constants.tsx
@@ -1,1 +1,20 @@
 export const API = 'http://localhost:3000/api'
+
+export const AccessChat = async(token = '', id = '') => {
+
+    if(token.length === 0){
+        return
+    }
+
+    const response = await fetch(`${API}/chat`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({friendId: id})
+    })
+
+    const chat = await response.json();
+    return chat;
+}
