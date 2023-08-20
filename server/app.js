0b91f40a4fd42983660d54ceb4d2127d118266d6
Arturo Rivera <arturo.riverar97@gmail.com>
2023-08-20 10:31:15 -0600

    Add message routes to app object

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const userRouter = require('./routes/user');
 const chatRouter = require('./routes/chat');
+const messageRouter = require('./routes/message');
 const mongoose = require('mongoose')
 
 var app = express();
@@ -48,6 +49,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/user', userRouter);
 app.use('/api/chat', chatRouter);
+app.use('/api/message', messageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
