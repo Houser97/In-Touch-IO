b3651c7785fa35819e38daaa2ab22340c1af356c
Arturo Rivera <arturo.riverar97@gmail.com>
2023-08-20 20:15:39 -0600

    Add ChatAndMessages controller to route

--- server/routes/message.js
+++ server/routes/message.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const app = require('../app');
+const { ChatAndMessages } = require('../controllers/chatController');
 const { sendMessage } = require('../controllers/messageController');
 const { validate_token } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.post('/', validate_token, sendMessage);
-router.get('/:chatId', validate_token);
+router.get('/:chatId', validate_token, ChatAndMessages);
 
 module.exports = router;
