e8ac592e4a2f7093b8c5789b817984b1db962b05
Arturo Rivera <arturo.riverar97@gmail.com>
2023-08-20 20:16:36 -0600

    Create sendMessage function to create messages when submitting form

--- client/src/components/Chat/MessageInput.tsx
+++ client/src/components/Chat/MessageInput.tsx
@@ -1,9 +1,27 @@
+import { FormEvent, useState } from 'react'
+import { API } from '../../assets/constants';
 import '../../styles/Chat/MessageInput.css'
 
-const MessageInput = () => {
+const MessageInput = ({chatId = ''}) => {
+
+  const [message, setMessage] = useState('');
+
+  const sendMessage = (e:FormEvent) => {
+    e.preventDefault();
+    const token = JSON.parse(localStorage.getItem('token') || '')
+    fetch(`${API}/message`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }, 
+      body: JSON.stringify({content: message, chatId})
+    });
+  }
+
   return (
-    <form className='message__form'>
-        <input type="text" className='message__input' name='message' />
+    <form className='message__form' onSubmit={sendMessage}>
+        <input type="text" className='message__input' name='message' onChange={(e) => setMessage(e.target.value)}/>
         <button type='submit' className='send__message'></button>
     </form>
   )
