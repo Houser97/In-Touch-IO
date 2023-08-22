00ddcd7662704575635894ed507e82106604dafa
Arturo Rivera <arturo.riverar97@gmail.com>
2023-08-21 20:03:03 -0600

    Remove friends field and related virtual property

--- server/models/user.js
+++ server/models/user.js
@@ -6,20 +6,7 @@ const UserSchema = Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     pictureUrl: {type: String, default: ""},
-    publicId: {type: String},
-    friends: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    }]
+    publicId: {type: String}
 });
 
 module.exports = mongoose.model("User", UserSchema);
-
-
-// Definir la referencia después de definir el modelo User
-UserSchema.virtual('friendList', {
-    ref: 'User', // Modelo al que estás haciendo referencia
-    localField: '_id', // Campo local en User (ID del usuario)
-    foreignField: 'friends', // Campo en el modelo referenciado que corresponde a friends
-    justOne: false // Puede haber múltiples amigos, no solo uno
-});
