08490db8c669df1593042d4a0f9076bfb7338e50
Arturo Rivera <arturo.riverar97@gmail.com>
2023-08-27 09:49:07 -0600

    Add ScrollableFeed to show last messages in Chat

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import ScrollableFeed from 'react-scrollable-feed'
 import { chatContext } from '../App'
 import '../styles/Chat.css'
 import Header from './Chat/Header'
@@ -6,20 +7,20 @@ import Message from './Chat/Message'
 import MessageInput from './Chat/MessageInput'
 
 const Chat = () => {
-
-    const { openChat, chatData } = useContext(chatContext);
+  
+    const { openChat, messages, chatData } = useContext(chatContext);
     const userId = JSON.parse(localStorage.getItem('idInTouch') || "");
 
   return (
     <div className={`chat__container ${openChat && 'show-chat'}`}>
         <Header />
-        <div className='chat__body'>
-          {chatData.messages.length ? chatData.messages.map(({sender, content, createdAt}) => {
+        <ScrollableFeed className='chat__body'>
+          {messages.length ? messages.map(({sender, content, createdAt, _id}) => {
             return(
-              <Message owner={userId === sender._id} hour={createdAt} content={content} key={sender._id}/>
+              <Message owner={userId === sender._id} hour={createdAt} content={content} key={_id}/>
             )
           }) : ''}
-        </div>
+        </ ScrollableFeed>
         <MessageInput chatId={chatData.id}/>
     </div>
   )
