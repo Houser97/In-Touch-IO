33a58c8635ff7da10d7ebd36f79a61e4dd1c8f2e
Arturo Rivera <arturo.riverar97@gmail.com>
2023-08-26 23:53:38 -0600

    Replace chatData with messages and add message id as key

--- client/src/components/Chat/MessageInput.tsx
+++ client/src/components/Chat/MessageInput.tsx
@@ -1,10 +1,12 @@
-import { FormEvent, useState } from 'react'
+import { FormEvent, useContext, useState } from 'react'
+import { chatContext } from '../../App';
 import { API } from '../../assets/constants';
 import '../../styles/Chat/MessageInput.css'
 
 const MessageInput = ({chatId = ''}) => {
 
   const [message, setMessage] = useState('');
+  const { socket, setIsNewMessage } = useContext(chatContext)
 
   const sendMessage = (e:FormEvent) => {
     e.preventDefault();
@@ -16,6 +18,9 @@ const MessageInput = ({chatId = ''}) => {
         'Authorization': `Bearer ${token}`
       }, 
       body: JSON.stringify({content: message, chatId})
+    }).then(data => data.json()).then((message) => {
+      socket.emit('new message', message)
+      setIsNewMessage(prev => !prev)
     });
   }
 
