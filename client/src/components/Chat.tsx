c61cab2708c910994049e3498d1455762875aef8
Arturo Rivera <arturo.riverar97@gmail.com>
2023-09-01 19:47:15 -0600

    Add logic to display loading component or messages

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -6,23 +6,36 @@ import Header from './Chat/Header'
 import Message from './Chat/Message'
 import MessageInput from './Chat/MessageInput'
 import { DateTime } from 'luxon';
+import { loadingParams } from '../assets/constants'
+import Loading from './Loading'
 
 const Chat = () => {
   
     const { openChat, messages, chatData } = useContext(chatContext);
     const userId = JSON.parse(localStorage.getItem('idInTouch') || "");
+    const isLoading = messages[0].sender._id === '';
+
+    const LoadingSection = () => 
+      loadingParams.map((params) => {
+        return(
+          <Loading {...params} />
+        )
+    })
+
+    const messagesSection = () =>  
+      <ScrollableFeed>
+        {messages.length ? messages.map(({sender, content, createdAt, _id}) => {
+          const formattedTime = DateTime.fromISO(createdAt).toLocaleString(DateTime.TIME_SIMPLE);
+          return(
+            <Message owner={userId === sender._id} hour={formattedTime} content={content} key={_id}/>
+          )
+        }) : ''}
+      </ ScrollableFeed>
 
   return (
     <div className={`chat__container ${openChat && 'show-chat'}`}>
         <Header />
-        <ScrollableFeed>
-          {messages.length ? messages.map(({sender, content, createdAt, _id}) => {
-            const formattedTime = DateTime.fromISO(createdAt).toLocaleString(DateTime.TIME_SIMPLE);
-            return(
-              <Message owner={userId === sender._id} hour={formattedTime} content={content} key={_id}/>
-            )
-          }) : ''}
-        </ ScrollableFeed>
+        {isLoading ? <div className='loading__container'>{LoadingSection()}</div> : messagesSection()}
         <MessageInput chatId={chatData.id}/>
     </div>
   )
