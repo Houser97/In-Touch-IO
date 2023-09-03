ab24097917ef4254d9f5dcf3506eded0c5e5639e
Arturo Rivera <arturo.riverar97@gmail.com>
2023-09-03 08:50:17 -0600

    Set clearChat function to close chat and clear chatData state

--- client/src/components/Chat/Header.tsx
+++ client/src/components/Chat/Header.tsx
@@ -5,11 +5,16 @@ import Contact from '../Contact'
 
 const Header = () => {
 
-    const { chatData, setOpenChat } = useContext(chatContext)
+    const { chatData, setOpenChat, setChatData } = useContext(chatContext)
+
+    const clearChat = () => {
+      setOpenChat(false)
+      setChatData({id: '', name: '', image: ''})
+    }
 
   return (
     <header className='header__chat'>
-        <svg onClick={() => setOpenChat(false)} className='arrow-left' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>arrow-left</title><path d="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z" /></svg>
+        <svg onClick={clearChat} className='arrow-left' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>arrow-left</title><path d="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z" /></svg>
         <Contact picture={chatData.image} name={chatData.name} />
     </header>
   )
