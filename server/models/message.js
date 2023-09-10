f3d227bc77e6246dd8fd362cb2c5c99f5852110d
Arturo Rivera <arturo.riverar97@gmail.com>
2023-09-09 22:57:21 -0600

    Add isSeen field to message model
    This field indicated whether the receiver of the messsage has already
    seen the message, which helps to show the amount of new messages in the
    client.

--- server/models/message.js
+++ server/models/message.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const MessageSchema = Schema({
     sender: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
     content: {type: String, trim: true},
-    chat: {type: mongoose.Schema.Types.ObjectId, ref: "Chat"}
+    chat: {type: mongoose.Schema.Types.ObjectId, ref: "Chat"},
+    isSeen: {type: Boolean, default: false}
 },
 {
     timestamps:true
