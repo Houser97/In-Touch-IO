16a72fe5c6dc1eca3654465d9237821e9ad8701c
Arturo Rivera <arturo.riverar97@gmail.com>
2023-09-09 22:59:07 -0600

    Change /:chatId route to post method
    This route will fetch all mesages related to the selected chat. On the
    other hand, it will set messages whose isSeen field is false to true,
    since the receiver has seen those messages as soon as she/he has opened
    the chat.

--- server/routes/message.js
+++ server/routes/message.js
@@ -4,6 +4,6 @@ const { validate_token } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.post('/', validate_token, sendMessage);
-router.get('/:chatId', validate_token, chatMessages);
+router.post('/:chatId', validate_token, chatMessages); // Buscar mensajes de un chat y coloca mensajes no vistos en vistos.
 
 module.exports = router;
