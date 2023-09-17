33d0fd13f8fd7134d5b391b35568e47bf38ec5fa
Arturo Rivera <arturo.riverar97@gmail.com>
2023-09-17 15:28:23 -0600

    Add default image if users dont choose one

--- server/models/user.js
+++ server/models/user.js
@@ -5,7 +5,7 @@ const UserSchema = Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    pictureUrl: {type: String, default: ""},
+    pictureUrl: {type: String, default: "https://res.cloudinary.com/dluwqcce9/image/upload/v1694961227/InTouch/qqaarw68ruwwluvcphkh.jpg"},
     publicId: {type: String}
 });
 
