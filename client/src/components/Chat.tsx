a95d2bcd7b668b673f9c9a30c6713ac902c292f6
Arturo Rivera <arturo.riverar97@gmail.com>
2023-09-17 09:13:24 -0600

    Import ImagePreview and declare states to handle sending of images

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import ScrollableFeed from 'react-scrollable-feed'
 import { chatContext, generalContext, messagesContext } from '../App'
 import '../styles/Chat.css'
@@ -8,6 +8,7 @@ import MessageInput from './Chat/MessageInput'
 import { DateTime } from 'luxon';
 import { loadingParams } from '../assets/constants'
 import Loading from './Loading'
+import ImagePreview from './Chat/ImagePreview'
 
 const Chat = () => {
   
@@ -18,6 +19,10 @@ const Chat = () => {
     // Se comprueba que haya mensajes para verificar que no se trate de un chat nuevo.
     const isLoading = messages.length && messages[0].sender._id === '';
 
+    const [fileInputState, setFileInputState] = useState('');
+    const [previewSource, setPreviewSource] = useState('');
+    const [selectedFile, setSelectedFile] = useState<Blob | null>(null);
+
     const LoadingSection = () => 
       loadingParams.map((params, index) => {
         return(
@@ -27,19 +32,22 @@ const Chat = () => {
 
     const messagesSection = () =>  
       <ScrollableFeed>
-        {messages.length ? messages.map(({sender, content, createdAt, _id}) => {
+        {messages.length ? messages.map(({sender, content, createdAt, _id, image}) => {
           const formattedTime = DateTime.fromISO(createdAt).toLocaleString(DateTime.TIME_SIMPLE);
           return(
-            <Message owner={userId === sender._id} hour={formattedTime} content={content} key={_id}/>
+            <Message owner={userId === sender._id} hour={formattedTime} content={content} key={_id} image={image} />
           )
         }) : ''}
       </ ScrollableFeed>
 
+      const MessageInputProps = {fileInputState, previewSource, selectedFile, setFileInputState, setPreviewSource, setSelectedFile, chatId: chatData.id }
+
   return (
     <div className={`chat__container ${openChat && 'show-chat'}`}>
         <Header />
+        <ImagePreview previewSource={previewSource} setPreviewSource={setPreviewSource} setSelectedFile={setSelectedFile} setFileInputState={setFileInputState}/>
         {isLoading ? <div className='loading__container'>{LoadingSection()}</div> : messagesSection()}
-        <MessageInput chatId={chatData.id}/>
+        <MessageInput {...MessageInputProps}/>
     </div>
   )
 }
