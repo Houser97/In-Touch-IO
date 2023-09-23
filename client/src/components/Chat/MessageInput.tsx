a4ae272dfd4a5abfcf3a2552ebfe75155ac49a81
Arturo Rivera <arturo.riverar97@gmail.com>
2023-09-23 09:20:43 -0600

    Disable image input when an image is selected

--- client/src/components/Chat/MessageInput.tsx
+++ client/src/components/Chat/MessageInput.tsx
@@ -96,7 +96,7 @@ const previewFile = (file: File) => {
 
   return (
     <form className='message__form' onSubmit={handleMessageCreation}>
-        <label className="message__img-label" htmlFor="file">
+        <label className={`message__img-label ${selectedFile && 'hide__input-img'}`} htmlFor="file">
             <svg viewBox="0 0 24 24" className='image_svg'><title>image-area</title><path d="M20,5A2,2 0 0,1 22,7V17A2,2 0 0,1 20,19H4C2.89,19 2,18.1 2,17V7C2,5.89 2.89,5 4,5H20M5,16H19L14.5,10L11,14.5L8.5,11.5L5,16Z" /></svg>
             <input 
               type="file" 
