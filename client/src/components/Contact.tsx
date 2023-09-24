e7bdcbea8afa2b1ed4dd4ed0adfa32b4baa1ae91
Arturo Rivera <arturo.riverar97@gmail.com>
2023-09-23 21:45:20 -0600

    Add tick svg and 'added' prop
    The 'added' prop is used in the Search component to dynamically
    determine whether the green tick should appear depending on whether
    the user is already in the friend list.

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -4,7 +4,7 @@ import { generalContext } from '../App'
 import { AccessChat, checkLocalStorage } from '../assets/constants'
 import '../styles/Contact.css'
 
-const Contact = ({picture = '', name = '', id = ''}) => {
+const Contact = ({picture = '', name = '', id = '', added = false}) => {
 
   const { setUpdateChats } = useContext(generalContext)
   const navigate = useNavigate()
@@ -23,6 +23,7 @@ const Contact = ({picture = '', name = '', id = ''}) => {
 
   return (
     <div className="contact" onClick={handleClick}>
+        <svg className={`tick ${added && 'show-added'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>check-circle</title><path d="M12 2C6.5 2 2 6.5 2 12S6.5 22 12 22 22 17.5 22 12 17.5 2 12 2M10 17L5 12L6.41 10.59L10 14.17L17.59 6.58L19 8L10 17Z" /></svg>
         <img className='contact__picture' src={picture} alt="contact-picture" />
         {name.length !== 0 && <div className="contact__name">{name}</div>}
     </div>
