c0df627c0c5326a2d96acd74314a40f9888fe84a
Arturo Rivera <arturo.riverar97@gmail.com>
2023-09-23 19:17:56 -0600

    Set text prop to edit the description displayed by AddButton

--- client/src/components/AddButton.tsx
+++ client/src/components/AddButton.tsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { generalContext } from '../App';
 import '../styles/AddButton.css'
 
-const AddButton = () => {
+const AddButton = ({text = ''}) => {
 
     const { setOpenSearch } = useContext(generalContext);
 
@@ -11,7 +11,7 @@ const AddButton = () => {
         <div className='contact__picture svg__search-container'>
             <svg className='search__svg' viewBox="0 0 24 24"><title>plus</title><path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg>
         </div>
-        <div className="contact__name">Add</div>
+        <div className="contact__name">{text}</div>
     </div>
   )
 }
