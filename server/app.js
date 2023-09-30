5fbca9d9d103c282b42a3380fc2a9e849ddbbafc
Arturo Rivera <arturo.riverar97@gmail.com>
2023-09-30 12:44:06 -0600

    Catch all routes to render index.html in public

--- server/app.js
+++ server/app.js
@@ -51,6 +51,10 @@ app.use('/api/user', userRouter);
 app.use('/api/chat', chatRouter);
 app.use('/api/message', messageRouter);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
