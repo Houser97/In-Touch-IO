49e30452c5b0c1ed1b1ef8acafc4ab2bcf408b10
Arturo Rivera <arturo.riverar97@gmail.com>
2023-10-08 10:41:49 -0600

    Add logic to update and read isDark variable in local storage

--- client/src/hooks/useDarkMode.tsx
+++ client/src/hooks/useDarkMode.tsx
@@ -1,9 +1,17 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 const useDarkMode = () => {
 
-    const [isDark, setIsDark] = useState(true)
+    const [isDark, setIsDark] = useState(() => {
+        // Verifica si existe una variable en el Local Storage para determinar el valor inicial
+        const localStorageValue = localStorage.getItem('isDark');
+        return localStorageValue ? JSON.parse(localStorageValue) : false;
+      });
+
+    useEffect(() => {
+        localStorage.setItem('isDark', JSON.stringify(isDark));
+    }, [isDark]);
 
   return [isDark, setIsDark]
 }
