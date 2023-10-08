7ffa98c2c540351385f0e114b66b75fc1d3dc7ba
Arturo Rivera <arturo.riverar97@gmail.com>
2023-10-08 13:29:29 -0600

    Create ToggleDarkBtn component to switch theme

---
+++ client/src/components/ToggleDarkBtn.tsx
@@ -0,0 +1,16 @@
+import { useContext } from 'react';
+import { generalContext } from '../App';
+import '../styles/ToggleDarkBtn.css'
+
+const ToggleDarkBtn = () => {
+
+  const {isDark, setIsDark} = useContext(generalContext);
+
+  return (
+    <div className={`toggle ${isDark && 'active'}`} onClick={() => setIsDark(!isDark)}>
+      <i className='indicator'></i>
+    </div>
+  )
+}
+
+export default ToggleDarkBtn
