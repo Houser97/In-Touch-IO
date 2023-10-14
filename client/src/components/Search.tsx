440581f97d901bc66ce9ba24f6e403d2d4c58408
Arturo Rivera <arturo.riverar97@gmail.com>
2023-10-13 19:55:29 -0600

    Add SearchLoader component and implement FetchResult function

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -5,6 +5,7 @@ import { API, checkLocalStorage } from '../assets/constants'
 import { user_type } from '../TypeScript/typesApp'
 import '../styles/Search.css'
 import Contact from './Contact'
+import SearchLoader from './Loaders/SearchLoader'
 
 const fetchUsers = async(query:string) => {
   const token = JSON.parse(localStorage.getItem("token") || '');
@@ -40,6 +41,7 @@ const Search = () => {
   
   const [userSearchResult, setUserSearchResult] = useState([]);
   const [query, setQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const initialFetch = async() => {
@@ -48,7 +50,8 @@ const Search = () => {
         navigate('/')
         return;
       }
-      setUserSearchResult(users)
+      setUserSearchResult(users);
+      setIsLoading(false);
     }
 
     const timeoutId = setTimeout(initialFetch, 2000);
@@ -72,6 +75,27 @@ const Search = () => {
     setUserSearchResult(users)
   }
 
+  const FetchResult = () => {
+    return(
+      isLoading ? 
+      <div className='loader__container'>
+        <SearchLoader />
+      </div>
+      :  <div className="search__result">
+            {
+              userSearchResult.length
+              ? userSearchResult.map(({name, pictureUrl, _id}) => {
+                return(
+                  <Contact name={name} picture={pictureUrl} id={_id} key={_id} added={AddedFriends.includes(_id)} />
+                )
+              })
+              
+              : 'No users were found'
+            }
+          </div>
+    )
+  }
+
   return (
     <div className={`search__container ${openSearch && 'show-search'}`}>
         <svg onClick={() => setOpenSearch(false)} className='arrow-left' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>arrow-left</title><path d="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z" /></svg>
@@ -79,18 +103,7 @@ const Search = () => {
             <svg className="search__icon" aria-hidden="true" viewBox="0 0 24 24"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
             <input placeholder="Search" type="search" className="search__input" onChange={(e) => setQuery(e.target.value)} />
         </form>
-        <div className="search__result">
-          {
-            userSearchResult.length
-            ? userSearchResult.map(({name, pictureUrl, _id}) => {
-              return(
-                <Contact name={name} picture={pictureUrl} id={_id} key={_id} added={AddedFriends.includes(_id)} />
-              )
-            })
-            
-            : 'No users were found'
-          }
-        </div>
+        {FetchResult()}
     </div>
   )
 }
