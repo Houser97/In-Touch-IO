4c65d4f23d9be87da2307f5b887c659fdc4225e3
Arturo Rivera <arturo.riverar97@gmail.com>
2024-10-06 11:25:41 -0600

    Create axios adapter pattern

---
+++ client/src/presentation/config/api/inTouchIoApi.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const inTouchIoApi = axios.create({
+    baseURL: 'http://localhost:3002/api'
+});
+
+inTouchIoApi.interceptors.request.use(config => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    } else {
+        config.headers.Authorization = '';
+    }
+
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+
+export default inTouchIoApi;
