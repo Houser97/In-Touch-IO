e4ba0247e05c139f7df77eb04d6f2f9bca5c866d
Arturo Rivera <arturo.riverar97@gmail.com>
2024-10-12 23:40:39 -0600

    Create message dto to create messages

---
+++ server/src/domain/dtos/messages/create-message.dto.ts
@@ -0,0 +1,19 @@
+export class CreateMessageDto {
+    private constructor(
+        public readonly sender: string,
+        public readonly content: string,
+        public readonly chat: string,
+        public readonly image: string,
+    ) { }
+
+    static create(props: { [key: string]: string }): [string?, CreateMessageDto?] {
+        const { sender, content, chat, image } = props;
+
+        if (!sender) return ['Missing sender'];
+        if (!content) return ['Missing content'];
+        if (!chat) return ['Missing chat'];
+        if (image !== undefined && typeof (image) !== 'string') return ['Missing must be a string'];
+
+        return [undefined, new CreateMessageDto(sender, content, chat, image)];
+    }
+}
