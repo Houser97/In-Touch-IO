4ec5ba0c53948cd3085f8089bdff5e1e79f3d01a
Arturo Rivera <arturo.riverar97@gmail.com>
2024-10-27 17:59:29 -0600

    Remove upload image logic and set oldPublicId

--- server/src/presentation/services/user.service.ts
+++ server/src/presentation/services/user.service.ts
@@ -17,23 +17,18 @@ export class UserService {
         }
     }
 
-    async update(id: string, updateUserDto: UpdateUserDto) {
+    async update(id: string, oldPublicId: string, updateUserDto: UpdateUserDto) {
 
         const userExists = await UserModel.findById(id);
 
         if (!userExists) throw CustomError.badRequest('User does not exists');
 
         try {
-            const { pictureUrl, pictureId } = updateUserDto;
+            // const { secure_url, public_id } = await CloudinaryAdapter.uploadImage(pictureUrl); // Upload is made in frontend
 
-            const { secure_url, public_id } = await CloudinaryAdapter.uploadImage(pictureUrl);
-
-            if (pictureId !== 'default') {
-                await CloudinaryAdapter.deleteImageByPublicId(pictureId);
+            if (oldPublicId !== 'default') {
+                await CloudinaryAdapter.deleteImageByPublicId(oldPublicId);
             }
-
-            updateUserDto.pictureUrl = secure_url;
-            updateUserDto.pictureId = public_id;
 
             const user = await UserModel.findByIdAndUpdate(id, updateUserDto, { new: true });
             return UserEntity.fromObject(user!.toObject());
