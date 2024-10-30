d888c2687e7c2853a59177622269ea874e6872d0
Arturo Rivera <arturo.riverar97@gmail.com>
2024-10-29 19:58:26 -0600

    React to personal-message event

--- server/src/config/sockets.ts
+++ server/src/config/sockets.ts
@@ -1,8 +1,14 @@
 import { Server } from 'socket.io';
+import { MessageService } from '../presentation/services/message.service';
+import { CreateMessageDto } from '../domain/dtos/messages/create-message.dto';
+import { UpdateChatDto } from '../domain/dtos/chats/update-chat.dto';
+import { ChatService } from '../presentation/services/chat.service';
 
 export class Sockets {
     constructor(
         private readonly io: Server,
+        private readonly messageService: MessageService,
+        private readonly chatService: ChatService,
     ) {
         this.socketEvents();
         // io.listen(3002);
@@ -11,23 +17,45 @@ export class Sockets {
     socketEvents() {
         this.io.on('connect', (socket) => {
             console.log('connected to socket.io');
+
             // New room using id of user.
             socket.on('setup', (userId) => {
                 socket.join(userId);
             })
-            // // New room using id of user.
-            // socket.on('join chat', (room) => {
-            //     socket.join(room);
-            // })
 
-            socket.on('new message', (newMessage) => {
-                const chat = newMessage.chat
+            // New room using id of user.
+            socket.on('join-chat', (chatId) => {
+                socket.join(chatId);
+            })
 
-                if (!chat.users.length) return
+            socket.on('personal-message', async (payload) => {
+                const { chat, message } = payload;
+                const [error, createMessageDto] = CreateMessageDto.create(message);
 
-                chat.users.forEach((user: { _id: string | string[]; }) => {
-                    socket.in(user._id).emit('message received', newMessage)
-                })
+                if (error) {
+                    console.log(error);
+                    this.io.to(message.sender).emit('personal-message', error);
+                    return;
+                }
+
+                try {
+                    const message = await this.messageService.create(createMessageDto!);
+
+                    const { id } = message;
+                    const [_, updateChatDto] = UpdateChatDto.create({ lastMessage: id });
+                    await this.chatService.update(chat.id, updateChatDto!)
+                    const friendId = chat.users.filter((user: any) => user.id != message.sender)[0].id
+                    const updatedChat = await this.chatService.getById(chat.id, friendId);
+
+                    chat.users.forEach((user: any) => {
+                        this.io.to(user.id).emit('personal-message', { message, chat: updatedChat });
+                    });
+
+                } catch (error) {
+                    console.log(error)
+                    this.io.to(message.sender).emit('personal-message', error);
+                }
+
             })
 
             socket.off("setup", () => {
