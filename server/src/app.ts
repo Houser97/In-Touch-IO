f267293a2479b2f0e8fa32dec38c0d8074d39eb8
Arturo Rivera <arturo.riverar97@gmail.com>
2024-11-04 21:24:43 -0600

    Create server entry point

---
+++ server/src/app.ts
@@ -0,0 +1,22 @@
+import { envs } from "./config/envs";
+import { MongoDatabase } from "./data/mongo/mongo-database";
+import { AppRoutes } from "./presentation/routes";
+import { Server } from "./presentation/server";
+
+(async () => {
+    main();
+})();
+
+async function main() {
+    await MongoDatabase.connect({
+        mongoUrl: envs.MONGO_URL,
+        dbName: envs.MONGO_DB_NAME,
+    });
+
+    const server = new Server({
+        port: envs.PORT,
+        routes: AppRoutes.routes,
+    });
+
+    server.start();
+}
