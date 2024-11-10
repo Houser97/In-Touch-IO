d600eeab05cff7141116d4a7559378b3e7561469
Arturo Rivera <arturo.riverar97@gmail.com>
2024-11-10 08:49:19 -0600

    Create Socket Provider

---
+++ client/src/presentation/providers/SocketProvider.tsx
@@ -0,0 +1,40 @@
+import { createContext, PropsWithChildren, useContext, useEffect } from "react";
+import { AuthContext } from "./AuthProvider";
+import { Socket } from "socket.io-client";
+import { useSocket } from "../hooks/useSocket";
+
+interface SocketContextProps {
+    socket: Socket | null;
+    online: boolean;
+}
+
+export const SocketContext = createContext<SocketContextProps>({
+    socket: null,
+    online: false
+});
+
+export const SocketProvider = ({ children }: PropsWithChildren) => {
+    const { socket, online, connect, disconnect } = useSocket('http://localhost:3002');
+    const { auth } = useContext(AuthContext);
+
+    useEffect(() => {
+        if (auth.status === 'authenticated') {
+            connect();
+        }
+    }, [auth.status])
+
+    useEffect(() => {
+        if (auth.status !== 'authenticated') {
+            disconnect();
+        }
+    }, [auth.status])
+
+    return (
+        <SocketContext.Provider value={{
+            socket,
+            online
+        }}>
+            {children}
+        </SocketContext.Provider>
+    )
+}
