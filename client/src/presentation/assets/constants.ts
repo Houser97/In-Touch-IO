aa50d73b5bc593a37267ab87fab4b85deab51b3f
Arturo Rivera <arturo.riverar97@gmail.com>
2024-11-15 21:17:01 -0600

    Create images object

---
+++ client/src/presentation/assets/constants.ts
@@ -0,0 +1,5 @@
+export const images = {
+    'auth': 'https://res.cloudinary.com/dluwqcce9/image/upload/v1731377789/InTouch/Login_s4jnr0.jpg',
+    'favicon': 'https://res.cloudinary.com/dluwqcce9/image/upload/v1731377789/InTouch/Logo_p8vtgu.png',
+    'emptyChat': 'https://res.cloudinary.com/dluwqcce9/image/upload/v1731377789/InTouch/Messages_ws6u09.png'
+}
