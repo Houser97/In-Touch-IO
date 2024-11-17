a8fedee96b7b864a8fe4f6b8211cd48323af6c3b
Arturo Rivera <arturo.riverar97@gmail.com>
2024-11-17 13:41:03 -0600

    Add validation and file upload middlewares

---
+++ server/src/presentation/middlewares/file-upload.middleware.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from "express";
+
+export class FileUploadMiddleware {
+    static containFiles(req: Request, res: Response, next: NextFunction) {
+        if (!req.files || Object.keys(req.files).length === 0) return res.status(400).json({ error: 'No files were selected' });
+        req.body.pictureUrl = req.files.pictureUrl;
+
+        next();
+    }
+}
---
+++ server/src/presentation/middlewares/validators.middleware.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from "express";
+import { body, check, validationResult } from "express-validator";
+import { Validators } from "../../config/validators";
+
+export class ValidatorsMiddleware {
+
+    static isValidEmail() {
+        return check('email').isEmail().withMessage('Invalid Email')
+            .trim()
+            .escape()
+            .normalizeEmail();
+    }
+
+    static isValidPassword() {
+        return body('password')
+            .isLength({ min: 4 }).withMessage('Password must contain at least 4 characters.')
+            .matches('[A-Z]').withMessage('Password must contain at least 1 upper letter.')
+            .matches('[0-9]').withMessage('Password must contain at least 1 number.')
+            .trim()
+            .escape();
+    }
+
+    static validateFields(req: Request, res: Response, next: NextFunction) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const messages = errors.array().map(({ msg }) => msg);
+            return res.status(401).json({ error: messages });
+        }
+        next();
+    }
+
+    static validateMongoId(paramName: string) {
+        return (req: Request, res: Response, next: NextFunction) => {
+            const id = req.params[paramName];
+
+            if (!Validators.isMongoID(id))
+                return res.status(400).json({ error: 'Invalid Mongo Id' });
+            next();
+        }
+    }
+
+}
