13fdff1e11f8a5991129fd6b9bf464d0db682ea7
Arturo Rivera <arturo.riverar97@gmail.com>
2024-12-21 21:17:29 -0600

    Include app entry point

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,28 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App'
-import Authentication from './components/Authentication'
-import UpdateUser from './components/UpdateUser'
 import './index.css'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Authentication />,
-  },
-  {
-    path: '/chats',
-    element: <App />
-  },
-  {
-    path: '/settings/:id',
-    element: <UpdateUser />
-  }
-])
+import { InTouchIoApp } from './InTouchIoApp'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <InTouchIoApp />
   </React.StrictMode>,
 )
