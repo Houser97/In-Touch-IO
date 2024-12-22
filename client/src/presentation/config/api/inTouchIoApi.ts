b519c240c10517fcadfc02ca7c49c987fdb30596
Arturo Rivera <arturo.riverar97@gmail.com>
2024-12-21 22:04:49 -0600

    Fix api url

--- client/src/presentation/config/api/inTouchIoApi.ts
+++ client/src/presentation/config/api/inTouchIoApi.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const inTouchIoApi = axios.create({
-    /*baseURL: 'http://localhost:3002/api'*/
-    baseURL: 'https://in-touch-io.onrender.com//api'
+    /* baseURL: 'http://localhost:3002/api'*/
+    baseURL: 'https://in-touch-io.onrender.com/api'
 });
 
 inTouchIoApi.interceptors.request.use(config => {
