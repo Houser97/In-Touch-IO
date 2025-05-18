624f18d0940d4047bad7da232e70e0fd3cf64762
Arturo Rivera <arturo.riverar97@gmail.com>
2025-05-18 10:49:18 -0600

    Register database services

--- ServerNET/API/Program.cs
+++ ServerNET/API/Program.cs
@@ -1,9 +1,24 @@
+using Application.Messages;
+using Domain;
+using MongoDB.Bson.Serialization;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<AppDbContext>(
+BsonClassMap.RegisterClassMap<Message>(cm =>
+{
+    cm.AutoMap();
+    cm.SetIgnoreExtraElements(true);
+});
+
+// Configure settings inject IOptions<AppDbSettings>
+builder.Services.Configure<AppDbSettings>(
     builder.Configuration.GetSection("InTouchIoDatabase"));
+
+// Register AppDbContext as singleton
+builder.Services.AddSingleton<AppDbContext>();
+
+builder.Services.AddScoped<MessageService>();
 
 // Add services to the container.
 
