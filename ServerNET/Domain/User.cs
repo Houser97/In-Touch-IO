5491b62dc3178e9617fc898a12237f73466828dd
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-05-24 09:25:09 -0600

    Add missing fields

--- ServerNET/Domain/User.cs
+++ ServerNET/Domain/User.cs
@@ -1,8 +1,23 @@
 using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Domain;
 
 public class User
 {
-
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    [BsonElement("_id")]
+    public string? Id { get; set; }
+    [BsonElement("email")]
+    public string Email { get; set; } = null!;
+    [BsonElement("name")]
+    public string Name { get; set; } = null!;
+    [BsonElement("password")]
+    public string Password { get; set; } = null!;
+    [BsonElement("pictureUrl")]
+    public string PictureUrl { get; set; } = null!;
+    [BsonElement("pictureId")]
+    public string PictureId { get; set; } = null!;
 }
