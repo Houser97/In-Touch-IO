306df89dd2f16a44d0fa8c49356f913aa414e6fd
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-05-23 21:21:10 -0600

    Create JwtSettings.cs

---
+++ ServerNET/Application/Auth/JwtSettings.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Application.Auth;
+
+public class JwtSettings
+{
+    public string Key { get; set; } = null!;
+    public string Issuer { get; set; } = null!;
+    public string Audience { get; set; } = null!;
+    public int ExpiresInMinutes { get; set; }
+}
