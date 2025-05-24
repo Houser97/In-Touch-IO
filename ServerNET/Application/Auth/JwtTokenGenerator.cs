0ea3f107e6cf677feef0b949847a5a61b3f02966
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-05-23 21:21:52 -0600

    Create JwtTokenGenerator.cs

---
+++ ServerNET/Application/Auth/JwtTokenGenerator.cs
@@ -0,0 +1,38 @@
+using System;
+
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Domain;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Application.Auth;
+
+public class JwtTokenGenerator(IOptions<JwtSettings> options)
+{
+    private readonly JwtSettings _settings = options.Value;
+
+    public string GenerateToken(User user)
+    {
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Email), // Importante que user.Id sea string
+            new Claim(ClaimTypes.Email, user.Email),
+            new Claim(ClaimTypes.Name, user.Username) // opcional
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_settings.Key));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _settings.Issuer,
+            audience: _settings.Audience,
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(_settings.ExpiresInMinutes),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
