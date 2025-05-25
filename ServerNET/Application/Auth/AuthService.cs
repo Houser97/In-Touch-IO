e9afed8484ee6cef811633163c38d02604c0f920
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-05-25 11:18:01 -0600

    Create Register and Status methods

--- ServerNET/Application/Auth/AuthService.cs
+++ ServerNET/Application/Auth/AuthService.cs
@@ -48,4 +48,73 @@ public class AuthService(JwtTokenGenerator tokenGenerator, AppDbContext dbContex
         };
 
     }
+
+    public async Task<AuthResultDto> Register(RegisterUserDto registerUserDto)
+    {
+        var userExists = await _usersCollection
+            .Find(u => u.Email == registerUserDto.Email)
+            .FirstOrDefaultAsync();
+
+        if (userExists != null)
+        {
+            return new AuthResultDto
+            {
+                Success = false,
+                Message = "User already exists"
+            };
+        }
+
+        try
+        {
+            var user = new User
+            {
+                Email = registerUserDto.Email,
+                Name = registerUserDto.Name,
+                PictureUrl = registerUserDto.PictureUrl ?? string.Empty,
+                Password = BCrypt.Net.BCrypt.HashPassword(registerUserDto.Password),
+                PictureId = registerUserDto.PictureId ?? "default"
+            };
+
+            await _usersCollection.InsertOneAsync(user);
+
+            user.Password = null!;
+
+            var token = _tokenGenerator.GenerateToken(user);
+
+            return new AuthResultDto
+            {
+                Success = true,
+                Token = token,
+                User = user
+            };
+        }
+        catch (Exception ex)
+        {
+            return new AuthResultDto
+            {
+                Success = false,
+                Message = $"Internal server error: {ex.Message}"
+            };
+        }
+    }
+
+    public async Task<AuthResultDto> GetAuthenticatedUser(string userId)
+    {
+        var user = await _usersCollection.Find(u => u.Id == userId).FirstOrDefaultAsync();
+
+        if (user == null)
+        {
+            return new AuthResultDto
+            {
+                Success = false,
+                Message = "User not found",
+            };
+        }
+
+        return new AuthResultDto
+        {
+            Success = true,
+            User = user
+        };
+    }
 }
