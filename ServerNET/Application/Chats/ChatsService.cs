fb78c5dc3dbd815cf451b9f09e1da5250b3b5d5c
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-06-07 07:24:03 -0700

    Use pipelines to join data

--- ServerNET/Application/Chats/ChatsService.cs
+++ ServerNET/Application/Chats/ChatsService.cs
@@ -1,48 +1,60 @@
 using System;
 using Application.Core;
+using Application.DTOs;
+using Application.Messages;
 using Domain;
 using Microsoft.Extensions.Options;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using Persistence;
 
 namespace Application.Chats;
 
-public class ChatsService(AppDbContext dbContext, IOptions<AppDbSettings> settings)
+public class ChatsService(AppDbContext dbContext, IOptions<AppDbSettings> settings, MessageService messageService)
 {
     private readonly IMongoCollection<Chat> _chatsCollection = dbContext.Database.GetCollection<Chat>(settings.Value.ChatsCollectionName);
     private readonly IMongoCollection<User> _usersCollection = dbContext.Database.GetCollection<User>(settings.Value.UsersCollectionName);
     private readonly IMongoCollection<Message> _messagesCollection = dbContext.Database.GetCollection<Message>(settings.Value.MessagesCollectionName);
+    private readonly MessageService _messageService = messageService;
 
-    public async Task<Result<object>> GetById(string chatId)
+    public async Task<Result<object>> GetById(string chatId, string userId)
     {
         try
         {
-            var chat = await _chatsCollection.Find(c => c.Id == chatId).FirstOrDefaultAsync();
+            var pipeline = new[]
+            {
+                new BsonDocument("$match", new BsonDocument("_id", new ObjectId(chatId))),
+                new BsonDocument("$lookup", new BsonDocument
+                {
+                    { "from", "users" },
+                    { "localField", "users" },
+                    { "foreignField", "_id" },
+                    { "as", "users" }
+                }),
+                new BsonDocument("$lookup", new BsonDocument
+                {
+                    {"from", "messages"},
+                    {"localField", "lastMessage"},
+                    {"foreignField", "_id"},
+                    {"as", "lastMessage"}
+                }),
+                new BsonDocument("$unwind", new BsonDocument
+                {
+                    {"path", "$lastMessage"},
+                    {"preserveNullAndEmptyArrays", true}
+                })
+            };
+
+            var chat = await _chatsCollection.Aggregate<BsonDocument>(pipeline).FirstOrDefaultAsync();
             if (chat == null)
                 return Result<object>.Failure("Chat not found", 404);
 
-            var userObjectIds = chat.Users.Select(id => id).ToList();
-            var users = await _usersCollection
-                .Find(u => userObjectIds.Contains(u.Id!))
-                .Project(u => new { u.Id, u.Name, u.Email, u.PictureUrl, u.PictureId })
-                .ToListAsync();
-
-            object? lastMessage = null;
-            if (!string.IsNullOrEmpty(chat.LastMessage))
-            {
-                lastMessage = await _messagesCollection
-                    .Find(m => m.Id == chat.LastMessage)
-                    .FirstOrDefaultAsync();
-            }
-
-            var unseenMessages = await GetUnseenMessages(chatId);
+            var unseenMessages = await _messageService.GetUnseenMessages([chatId], userId);
 
             var chatDto = new
             {
-                Chat = chat,
-                Users = users,
-                LastMessage = lastMessage,
-                UnseenMessages = unseenMessages
+                chat = ChatDTO.FromBson(chat),
+                unseenMessages = unseenMessages
             };
 
             return Result<object>.Success(chatDto);
@@ -53,11 +65,53 @@ public class ChatsService(AppDbContext dbContext, IOptions<AppDbSettings> settin
         }
     }
 
-    private async Task<List<Message>> GetUnseenMessages(string chatId)
+    public async Task<Result<object>> GetChatsByUserId(string userId)
     {
-        // Aquí puedes poner tu lógica real para obtener mensajes no vistos
-        return await _messagesCollection
-            .Find(m => m.Chat == chatId)
-            .ToListAsync();
+        try
+        {
+            var userObjectId = new ObjectId(userId);
+
+            var pipeline = new[]
+            {
+                new BsonDocument("$match", new BsonDocument("users", userObjectId)),
+                new BsonDocument("$lookup", new BsonDocument
+                {
+                    { "from", "users" },
+                    { "localField", "users" },
+                    { "foreignField", "_id" },
+                    { "as", "users" }
+                }),
+                new BsonDocument("$lookup", new BsonDocument
+                {
+                    { "from", "messages" },
+                    { "localField", "lastMessage" },
+                    { "foreignField", "_id" },
+                    { "as", "lastMessage" }
+                }),
+
+                new BsonDocument("$unwind", new BsonDocument
+                {
+                    { "path", "$lastMessage" },
+                    { "preserveNullAndEmptyArrays", true }
+                }),
+
+                new BsonDocument("$sort", new BsonDocument("updatedAt", -1))
+            };
+
+            var bsonChats = await _chatsCollection
+                .Aggregate<BsonDocument>(pipeline)
+                .ToListAsync();
+
+            var chats = bsonChats
+                .Select(ChatDTO.FromBson)
+                .ToList();
+
+            return Result<object>.Success(new { chats });
+
+        }
+        catch (Exception ex)
+        {
+            return Result<object>.Failure($"Internal server error: {ex.Message}", 500);
+        }
     }
 }
