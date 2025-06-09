174be14d3506b5db8a290d3aed6c8c93ffb69818
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-06-08 21:10:14 -0700

    Handle lastMessage null

--- ServerNET/Application/DTOs/ChatDTO.cs
+++ ServerNET/Application/DTOs/ChatDTO.cs
@@ -1,5 +1,6 @@
 using System;
-using Domain;
+using System.Collections.Generic;
+using System.Linq;
 using MongoDB.Bson;
 
 namespace Application.DTOs;
@@ -11,33 +12,37 @@ public class ChatDTO(string id, List<UserLikeDTO> users, object lastMessage, Dat
     public object LastMessage { get; set; } = lastMessage;
     public DateTime UpdatedAt { get; set; } = updatedAt;
 
-     public static ChatDTO FromBson(BsonDocument doc)
+    public static ChatDTO FromBson(BsonDocument doc)
     {
-        if (!doc.Contains("_id")) throw new ArgumentException("Missing _id");
-        if (!doc.Contains("users")) throw new ArgumentException("Missing users");
-        if (!doc.Contains("lastMessage")) throw new ArgumentException("Missing lastMessage");
-        if (!doc.Contains("updatedAt")) throw new ArgumentException("Missing updatedAt");
+        if (!doc.Contains("_id")) throw new ArgumentException("Missing _id from object");
+        if (!doc.Contains("users")) throw new ArgumentException("Missing users from object");
+        if (!doc.Contains("updatedAt")) throw new ArgumentException("Missing updatedAt from object");
 
         var id = doc["_id"].ToString();
 
         var usersArray = doc["users"].AsBsonArray;
         var users = usersArray
-            .Select(u => UserLikeDTO.FromBson(u.AsBsonDocument))
+            .Select(userDoc => UserLikeDTO.FromBson(userDoc.AsBsonDocument))
             .ToList();
 
-        object lastMessage;
-        var lastMessageRaw = doc["lastMessage"];
-        if (lastMessageRaw.IsBsonDocument)
+        object lastMessage = null!;
+
+        if (doc.Contains("lastMessage"))
         {
-            lastMessage = MessageDTO.FromBson(lastMessageRaw.AsBsonDocument);
-        }
-        else
-        {
-            lastMessage = lastMessageRaw.ToString()!; // en caso de que sea un string u otro tipo
+            var lastMessageRaw = doc["lastMessage"];
+            if (lastMessageRaw.IsBsonDocument)
+            {
+                lastMessage = MessageDTO.FromBson(lastMessageRaw.AsBsonDocument);
+            }
+            else
+            {
+                lastMessage = lastMessageRaw?.ToString() ?? string.Empty;
+            }
         }
 
         var updatedAt = doc["updatedAt"].ToUniversalTime();
 
         return new ChatDTO(id!, users, lastMessage, updatedAt);
     }
+
 }
