bfe04f77239d690be6275b0d1f5a99dce2ea32fb
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-06-14 13:08:41 -0700

    Implement GetUnseemMessages

--- ServerNET/Application/Messages/MessageService.cs
+++ ServerNET/Application/Messages/MessageService.cs
@@ -17,48 +17,91 @@ public class MessageService(AppDbContext dbContext, IOptions<AppDbSettings> sett
         return await _messagesCollection.Find(_ => true).ToListAsync();
     }
 
-public async Task<Result<object>> GetUnseenMessages(List<string> chatIds, string userId)
-{
-    try
+    public async Task<Result<object>> GetMessagesByChatId(string chatId, PaginationDto paginationDto)
     {
-        var filter = Builders<Message>.Filter.And(
-            Builders<Message>.Filter.Eq(m => m.IsSeen, false),
-            Builders<Message>.Filter.In(m => m.Chat, chatIds)
-            // Builders<Message>.Filter.Ne(m => m.Sender, userId)
-        );
-
-        var projection = Builders<Message>.Projection
-            .Include(m => m.Id)
-            .Include(m => m.Sender)
-            .Include(m => m.Chat);
-
-        var bsonMessages = await _messagesCollection
-            .Find(filter)
-            .Project<BsonDocument>(projection)
-            .ToListAsync();
-
-        var unseenMessagesByChat = new Dictionary<string, List<UnseenMessageDTO>>();
-
-        foreach (var doc in bsonMessages)
+        try
         {
-            var chatId = doc["chat"].AsObjectId.ToString();
-            var message = new UnseenMessageDTO
+            int page = paginationDto.Page;
+            int limit = paginationDto.Limit;
+
+            var filter = Builders<Message>.Filter.Eq(m => m.Chat, chatId);
+
+            var total = await _messagesCollection.CountDocumentsAsync(filter);
+
+            var messages = await _messagesCollection
+                .Find(filter)
+                .SortByDescending(m => m.CreatedAt)
+                .Skip((page - 1) * limit)
+                .Limit(limit)
+                .ToListAsync();
+
+            messages.Reverse();
+
+            var messagesDto = messages.Select(MessageDTO.FromEntity).ToList();
+
+            var totalPages = (int)Math.Ceiling((double)total / limit);
+
+            var result = new
             {
-                Id = doc["_id"].AsObjectId.ToString(),
-                Sender = doc["sender"].AsObjectId.ToString()
+                page,
+                limit,
+                total,
+                totalPages,
+                next = (page * limit < total) ? $"/api/messages/{chatId}?page={page + 1}&limit={limit}" : null,
+                prev = (page > 1) ? $"/api/messages/{chatId}?page={page - 1}&limit={limit}" : null,
+                messages = messagesDto
             };
+            return Result<object>.Success(result);
 
-            if (!unseenMessagesByChat.ContainsKey(chatId))
-                unseenMessagesByChat[chatId] = [];
-
-            unseenMessagesByChat[chatId].Add(message);
         }
+        catch (Exception ex)
+        {
+            return Result<object>.Failure($"Internal server error {ex.Message}", 500);
+        }
+    }
 
-        return Result<object>.Success(unseenMessagesByChat);
-    }
-    catch (Exception ex)
+    public async Task<Result<object>> GetUnseenMessages(List<string> chatIds, string userId)
     {
-        return Result<object>.Failure($"Internal server error: {ex.Message}", 500);
+        try
+        {
+            var filter = Builders<Message>.Filter.And(
+                Builders<Message>.Filter.Eq(m => m.IsSeen, false),
+                Builders<Message>.Filter.In(m => m.Chat, chatIds)
+            // Builders<Message>.Filter.Ne(m => m.Sender, userId)
+            );
+
+            var projection = Builders<Message>.Projection
+                .Include(m => m.Id)
+                .Include(m => m.Sender)
+                .Include(m => m.Chat);
+
+            var bsonMessages = await _messagesCollection
+                .Find(filter)
+                .Project<BsonDocument>(projection)
+                .ToListAsync();
+
+            var unseenMessagesByChat = new Dictionary<string, List<UnseenMessageDTO>>();
+
+            foreach (var doc in bsonMessages)
+            {
+                var chatId = doc["chat"].AsObjectId.ToString();
+                var message = new UnseenMessageDTO
+                {
+                    Id = doc["_id"].AsObjectId.ToString(),
+                    Sender = doc["sender"].AsObjectId.ToString()
+                };
+
+                if (!unseenMessagesByChat.ContainsKey(chatId))
+                    unseenMessagesByChat[chatId] = [];
+
+                unseenMessagesByChat[chatId].Add(message);
+            }
+
+            return Result<object>.Success(unseenMessagesByChat);
+        }
+        catch (Exception ex)
+        {
+            return Result<object>.Failure($"Internal server error: {ex.Message}", 500);
+        }
     }
-}
 }
