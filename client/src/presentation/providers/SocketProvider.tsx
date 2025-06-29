b677afec5e19f183f0dd1f3eb47a03b144ad11aa
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-06-29 09:55:59 -0700

    Update SocketProvider.tsx

--- client/src/presentation/providers/SocketProvider.tsx
+++ client/src/presentation/providers/SocketProvider.tsx
@@ -1,40 +1,43 @@
 import { createContext, PropsWithChildren, useContext, useEffect } from "react";
 import { AuthContext } from "./AuthProvider";
-import { Socket } from "socket.io-client";
+import { HubConnection } from "@microsoft/signalr";
 import { useSocket } from "../hooks/useSocket";
 
-interface SocketContextProps {
-    socket: Socket | null;
-    online: boolean;
+interface SignalRContextProps {
+  connection: HubConnection | null;
+  connected: boolean;
+  send: (method: string, ...args: any[]) => Promise<void>;
+  on: (
+    event: string,
+    callback: (...args: any[]) => void
+  ) => (() => void) | void;
 }
 
-export const SocketContext = createContext<SocketContextProps>({
-    socket: null,
-    online: false
+export const SocketContext = createContext<SignalRContextProps>({
+  connection: null,
+  connected: false,
+  send: async () => {},
+  on: () => {},
 });
 
 export const SocketProvider = ({ children }: PropsWithChildren) => {
-    const { socket, online, connect, disconnect } = useSocket('https://in-touch-io.onrender.com');
-    const { auth } = useContext(AuthContext);
+  const { connection, connected, connect, disconnect, send, on } = useSocket(
+    //"https://in-touch-io.onrender.com/chats"
+    "https://localhost:5001/signalR"
+  );
+  const { auth } = useContext(AuthContext);
 
-    useEffect(() => {
-        if (auth.status === 'authenticated') {
-            connect();
-        }
-    }, [auth.status])
+  useEffect(() => {
+    if (auth.status === "authenticated") {
+      connect();
+    } else {
+      disconnect();
+    }
+  }, [auth.status]);
 
-    useEffect(() => {
-        if (auth.status !== 'authenticated') {
-            disconnect();
-        }
-    }, [auth.status])
-
-    return (
-        <SocketContext.Provider value={{
-            socket,
-            online
-        }}>
-            {children}
-        </SocketContext.Provider>
-    )
-}
+  return (
+    <SocketContext.Provider value={{ connection, connected, send, on }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
