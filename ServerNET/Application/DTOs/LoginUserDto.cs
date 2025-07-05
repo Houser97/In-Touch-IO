e257554dc5d4929e5fadf882810f3a052d1a1da8
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-07-05 14:09:07 -0700

    Update LoginUserDto.cs
    Format email

--- ServerNET/Application/DTOs/LoginUserDto.cs
+++ ServerNET/Application/DTOs/LoginUserDto.cs
@@ -5,11 +5,18 @@ namespace Application.DTOs;
 
 public class LoginUserDto
 {
+    private string _email = null!;
+
     [Required(ErrorMessage = "Email is required")]
     [MinLength(3, ErrorMessage = "Email should have at least 3 characters")]
-    public string Email { get; set; } = null!;
+    public string Email
+    {
+        get => _email;
+        set => _email = value.ToLowerInvariant();
+    }
 
     [Required(ErrorMessage = "Password is required")]
     [MinLength(6, ErrorMessage = "Password should have at least 6 characters")]
     public string Password { get; set; } = null!;
 }
+
