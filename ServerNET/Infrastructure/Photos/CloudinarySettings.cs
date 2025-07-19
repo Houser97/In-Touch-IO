0a3ef2d1ea6ce68bd27c341b01aa64c2ccaa8c81
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-07-19 14:08:55 -0700

    Create CloudinarySettings.cs

---
+++ ServerNET/Infrastructure/Photos/CloudinarySettings.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Infrastructure.Photos;
+
+public class CloudinarySettings
+{
+    public required string CloudName { get; set; }
+    public required string ApiKey { get; set; }
+    public required string ApiSecret { get; set; }
+    public required string Preset { get; set; }
+}
