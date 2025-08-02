a69365157424b5e1315e7846de6d37edbd8fe98d
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-08-02 07:26:27 -0700

    Include oldPublicId in UpdateUserDto

--- ServerNET/Application/DTOs/Users/UpdateUserDto.cs
+++ ServerNET/Application/DTOs/Users/UpdateUserDto.cs
@@ -5,10 +5,12 @@ namespace Application.DTOs.Users;
 public class UpdateUserDto(
     string name,
     string pictureId,
-    string pictureUrl
+    string pictureUrl,
+    string oldPublicId
 )
 {
     public string Name { get; set; } = name;
     public string PictureId { get; set; } = pictureId;
     public string PictureUrl { get; set; } = pictureUrl;
+    public string OldPublicId { get; set; } = oldPublicId;
 }
