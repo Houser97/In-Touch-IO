74d6485bf4a8e27e2eda942e9fb405965e541a43
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-09-07 16:01:19 -0700

    Update Chat.cs
    Initialize Id with empty string

--- ServerNET/Domain/Chat.cs
+++ ServerNET/Domain/Chat.cs
@@ -9,7 +9,7 @@ public class Chat
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     [BsonElement("_id")]
-    public string? Id { get; set; }
+    public string Id { get; set; } = string.Empty;
     [BsonElement("users")]
     [BsonRepresentation(BsonType.ObjectId)]
     public required List<string> Users { get; set; }
