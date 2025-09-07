a85bc051121cb26b4f344dc8a4c0e31e1bd81e1e
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-09-07 16:02:04 -0700

    Inherit from BaseAuthDto

--- ServerNET/Application/DTOs/Auth/LoginUserDto.cs
+++ ServerNET/Application/DTOs/Auth/LoginUserDto.cs
@@ -1,17 +1,10 @@
 using System;
-using System.ComponentModel.DataAnnotations;
+using Application.Interfaces.Auth;
 
 namespace Application.DTOs.Auth;
 
-public class LoginUserDto
+public class LoginUserDto : BaseAuthDto
 {
 
-    [Required(ErrorMessage = "Email is required")]
-    [MinLength(3, ErrorMessage = "Email should have at least 3 characters")]
-    public string Email { get; set; } = null!;
-
-    [Required(ErrorMessage = "Password is required")]
-    [MinLength(6, ErrorMessage = "Password should have at least 6 characters")]
-    public string Password { get; set; } = null!;
 }
 
--- ServerNET/Application/DTOs/Auth/RegisterUserDto.cs
+++ ServerNET/Application/DTOs/Auth/RegisterUserDto.cs
@@ -1,22 +1,10 @@
 using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace Application.DTOs.Auth;
 
-public class RegisterUserDto
+public class RegisterUserDto : BaseAuthDto
 {
-    [Required(ErrorMessage = "Email is required")]
-    [EmailAddress]
-    public string Email { get; set; } = null!;
-
-    [Required(ErrorMessage = "Name is required")]
-    [MinLength(3, ErrorMessage = "Name should have at least 3 characters")]
     public string Name { get; set; } = null!;
-
-    [Required(ErrorMessage = "Password is required")]
-    [MinLength(6, ErrorMessage = "Password should have at least 6 characters")]
-    public string Password { get; set; } = null!;
-
     public string? PictureUrl { get; set; } = "https://res.cloudinary.com/dluwqcce9/image/upload/v1694961227/InTouch/qqaarw68ruwwluvcphkh.jpg";
     public string? PictureId { get; set; } = "default";
 }
