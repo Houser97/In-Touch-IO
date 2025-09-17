bd2fd80a7a4eeee0519b1a604b5f0d2bcae70c26
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-09-16 19:51:46 -0700

    Update UsersController.cs
    Create Update method

--- ServerNET/API/Controllers/UsersController.cs
+++ ServerNET/API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-using Application.Services.Users;
+using Application.DTOs.Users;
+using Application.Interfaces.Auth;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,15 +7,25 @@ namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UsersController(UserService userService) : ControllerBase
+    public class UsersController(IUserService userService) : ControllerBase
     {
-        private readonly UserService _userService = userService;
+        private readonly IUserService _userService = userService;
 
         [HttpGet()]
         [AllowAnonymous]
         public async Task<ActionResult> GetByNameOrEmail([FromQuery] string? search)
         {
             var result = await _userService.GetUserByNameOrEmail(search);
+
+            if (!result.IsSuccess) return StatusCode(result.Code, new { message = result.Error });
+
+            return Ok(result.Value);
+        }
+
+        [HttpPut("{userId}")]
+        public async Task<ActionResult> Update(string userId, UpdateUserDto updateUserDto)
+        {
+            var result = await _userService.Update(userId, updateUserDto);
 
             if (!result.IsSuccess) return StatusCode(result.Code, new { message = result.Error });
 
