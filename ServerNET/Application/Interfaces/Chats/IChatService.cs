e0fa6b43b562223804144e5abf22d59b5cb93cf0
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-09-16 19:53:07 -0700

    Update IChatService.cs
    Use ChatDto instead of generic object type

--- ServerNET/Application/Interfaces/Chats/IChatService.cs
+++ ServerNET/Application/Interfaces/Chats/IChatService.cs
@@ -6,8 +6,8 @@ namespace Application.Interfaces.Chats;
 
 public interface IChatService
 {
-    Task<Result<object>> GetById(string chatId, string userId);
+    Task<Result<ChatDTO>> GetById(string chatId, string userId);
     Task<Result<object>> GetChatsByUserId(string userId);
-    Task<Result<object>> CreateChat(CreateChatDto createChatDto, string userId);
-    Task<Result<object>> UpdateChat(string id, string userId, UpdateChatDto updateChatDto);
+    Task<Result<ChatDTO>> CreateChat(CreateChatDto createChatDto, string userId);
+    Task<Result<ChatDTO>> UpdateChat(string id, string userId, UpdateChatDto updateChatDto);
 }
