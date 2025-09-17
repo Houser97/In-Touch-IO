0c73aed76369529423162d45e9881e1335895cf9
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-09-16 19:52:24 -0700

    Update ChatDTO.cs
    Remove FromBson function

--- ServerNET/Application/DTOs/Chats/ChatDTO.cs
+++ ServerNET/Application/DTOs/Chats/ChatDTO.cs
@@ -1,47 +1,13 @@
-using System;
 using Application.DTOs.Messages;
 using Application.DTOs.Users;
-using MongoDB.Bson;
 
 namespace Application.DTOs.Chats;
 
-public class ChatDTO(string id, List<UserLikeDTO> users, object lastMessage, DateTime updatedAt)
+public class ChatDTO
 {
-    public string Id { get; set; } = id;
-    public List<UserLikeDTO> Users { get; set; } = users;
-    public object LastMessage { get; set; } = lastMessage;
-    public DateTime UpdatedAt { get; set; } = updatedAt;
-
-    public static ChatDTO FromBson(BsonDocument doc)
-    {
-        if (!doc.Contains("_id")) throw new ArgumentException("Missing _id from object");
-        if (!doc.Contains("users")) throw new ArgumentException("Missing users from object");
-        if (!doc.Contains("updatedAt")) throw new ArgumentException("Missing updatedAt from object");
-
-        var id = doc["_id"].ToString();
-
-        var usersArray = doc["users"].AsBsonArray;
-        var users = usersArray
-            .Select(userDoc => UserLikeDTO.FromBson(userDoc.AsBsonDocument))
-            .ToList();
-
-        object lastMessage = null!;
-
-        if (doc.Contains("lastMessage"))
-        {
-            var lastMessageRaw = doc["lastMessage"];
-            if (lastMessageRaw.IsBsonDocument)
-            {
-                lastMessage = MessageDTO.FromBson(lastMessageRaw.AsBsonDocument);
-            }
-            else
-            {
-                lastMessage = lastMessageRaw?.ToString() ?? string.Empty;
-            }
-        }
-
-        var updatedAt = doc["updatedAt"].ToUniversalTime();
-
-        return new ChatDTO(id!, users, lastMessage, updatedAt);
-    }
+    public string Id { get; set; } = string.Empty;
+    public List<UserDTO> Users { get; set; } = [];
+    public LastMessageDto? LastMessage { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public List<UnseenMessageDto> UnseenMessages { get; set; } = [];
 }
