30e7727a82b90aece50893ce8730f04d26c216a2
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-09-28 21:09:55 -0700

    Update UserDTO.cs

--- ServerNET/Application/DTOs/Users/UserDTO.cs
+++ ServerNET/Application/DTOs/Users/UserDTO.cs
@@ -1,12 +1,17 @@
 using System;
 using Domain;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Application.DTOs.Users;
 
 public class UserDTO
 {
-
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PictureUrl { get; set; }
+    public string PictureId { get; set; }
 }
 
 
