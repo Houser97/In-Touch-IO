4ce140b95a4d4e6644940e74ec6ef65f08ef6302
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-12-28 11:23:40 -0600

    Update SocketIoProvider.tsx

--- client/src/presentation/providers/SocketIoProvider.tsx
+++ client/src/presentation/providers/SocketIoProvider.tsx
@@ -1,40 +1,40 @@
-import { createContext, PropsWithChildren, useContext, useEffect } from "react";
-import { AuthContext } from "./AuthProvider";
-import { Socket } from "socket.io-client";
-import { useSocket } from "../hooks/useSocket";
+// import { createContext, PropsWithChildren, useContext, useEffect } from "react";
+// import { AuthContext } from "./AuthProvider";
+// import { Socket } from "socket.io-client";
+// import { useSocket } from "../hooks/useSocket";
 
-interface SocketContextProps {
-    socket: Socket | null;
-    online: boolean;
-}
+// interface SocketContextProps {
+//     socket: Socket | null;
+//     online: boolean;
+// }
 
-export const SocketContext = createContext<SocketContextProps>({
-    socket: null,
-    online: false
-});
+// export const SocketContext = createContext<SocketContextProps>({
+//     socket: null,
+//     online: false
+// });
 
-export const SocketProvider = ({ children }: PropsWithChildren) => {
-    const { socket, online, connect, disconnect } = useSocket('https://in-touch-io.onrender.com');
-    const { auth } = useContext(AuthContext);
+// export const SocketProvider = ({ children }: PropsWithChildren) => {
+//     const { socket, online, connect, disconnect } = useSocket('https://in-touch-io.onrender.com');
+//     const { auth } = useContext(AuthContext);
 
-    useEffect(() => {
-        if (auth.status === 'authenticated') {
-            connect();
-        }
-    }, [auth.status])
+//     useEffect(() => {
+//         if (auth.status === 'authenticated') {
+//             connect();
+//         }
+//     }, [auth.status])
 
-    useEffect(() => {
-        if (auth.status !== 'authenticated') {
-            disconnect();
-        }
-    }, [auth.status])
+//     useEffect(() => {
+//         if (auth.status !== 'authenticated') {
+//             disconnect();
+//         }
+//     }, [auth.status])
 
-    return (
-        <SocketContext.Provider value={{
-            socket,
-            online
-        }}>
-            {children}
-        </SocketContext.Provider>
-    )
-}
+//     return (
+//         <SocketContext.Provider value={{
+//             socket,
+//             online
+//         }}>
+//             {children}
+//         </SocketContext.Provider>
+//     )
+// }
